fa5fe4d51a38675fb6cb78988c13dd500760e40c
Brian Wood <bwood@berkeley.edu>
2018-01-01 20:38:18 -0800

    Fixes infinite loop, but results in 'no tab with [tabID]'

--- eventPage.js
+++ eventPage.js
@@ -1,17 +1,8 @@
-let exists = 1;
-
 chrome.webNavigation.onCommitted.addListener(function (data) {
-        //alert("here");
-        // chrome.tabs.create({'url': "http://berkeley.edu"});
-
         let windowData = {
-            url: data.url,
             tabId: data.tabId
         };
-
         chrome.windows.create(windowData);
-
-
     }
     , {
         url: [{hostSuffix: 'berkeley.edu'}]

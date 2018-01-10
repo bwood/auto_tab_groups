8e309d57c13cd28f65b344a9fa86a9dcd1994968
Brian Wood <bwood@berkeley.edu>
2018-01-09 17:16:42 -0800

    Begin creating object to keep track of window groups

--- eventPage.js
+++ eventPage.js
@@ -1,12 +1,44 @@
+let rules = {
+    group1: {
+        url: [
+            //{hostSuffix: 'berkeley.edu'},
+            //{hostSuffix: 'stanford.edu'}
+            // Match any file on hard drive. Use File>Open
+            {pathPrefix: '/Users'}
+        ]
+    }
+};
+
 chrome.webNavigation.onCompleted.addListener(function (data) {
-        let windowData = {
-            tabId: data.tabId
-        };
-        chrome.windows.create(windowData);
+        let groups = getGroups();
+        if (groups.group1 == undefined) {
+            chrome.windows.create({tabId: data.tabId}, function (window) {
+                setGroups('group1', window.id);
+            });
+        }
+        else {
+            chrome.tabs.move(data.tabId, {windowId: window.id});
+        }
     }
-    , {
-        url: [{hostSuffix: 'berkeley.edu'}]
-    }
+    , rules.group1
 );
 
+function getGroups() {
+    let groups = {};
+    chrome.storage.local.get('groups', function (items) {
+        //alert("Got " + items.groups);
+        if (items.groups) {
+            groups = items.groups;
+        }
+    });
+    return groups;
+}
 
+function setGroups(name, windowId) {
+    let groups = getGroups();
+    groups[name] = windowId;
+    chrome.storage.local.set({groups: groups});
+    //chrome.storage.local.get('groups', function (items) {
+//        alert("Got2 " + items.groups);
+//    })
+}
